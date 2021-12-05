f23932f2c697b4fe26f0277c06036aceeaab432d
Ivan Kalinin <i.kalinin@dexpa.io>
2021-12-05 22:50:03 +0300

    Add serialization and deserialization for control info

--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 #[derive(Debug, Clone, Copy)]
 pub struct PacketHeader {
     /// Packet sequence number
@@ -41,11 +39,37 @@ pub struct MessageDropRequestControlInfo {
     last_seq_no: u32,
 }
 
-/// Acknowledgement of Acknowledgement (ACK2) packet control info
-#[derive(Debug, Copy, Clone)]
-pub struct Ack2ControlInfo {
-    /// ACK packet seq. no.
-    pub seq_no: i32,
+impl MessageDropRequestControlInfo {
+    pub fn serialize<'a>(&self, buffer: &'a mut [u8]) -> Option<&'a [u8]> {
+        if buffer.len() < MDR_SIZE {
+            return None;
+        }
+
+        // 1) 32 bits: First sequence number in the message
+        buffer[0..4].copy_from_slice(&self.first_seq_no.to_le_bytes());
+
+        // 2) 32 bits: Last sequence number in the message
+        buffer[4..8].copy_from_slice(&self.last_seq_no.to_le_bytes());
+
+        Some(buffer.split_at(MDR_SIZE).0)
+    }
+
+    pub fn deserialize(buffer: &[u8]) -> Option<Self> {
+        if buffer.len() != MDR_SIZE {
+            return None;
+        }
+
+        // 1) 32 bits: First sequence number in the message
+        let first_seq_no = u32::from_le_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]);
+
+        // 2) 32 bits: Last sequence number in the message
+        let last_seq_no = u32::from_le_bytes([buffer[4], buffer[5], buffer[6], buffer[7]]);
+
+        Some(Self {
+            first_seq_no,
+            last_seq_no,
+        })
+    }
 }
 
 /// Negative-acknowledgment packet control info
@@ -61,14 +85,140 @@ pub enum NakControlInfo {
     },
 }
 
+impl NakControlInfo {
+    pub fn serialize<'a>(&self, buffer: &'a mut [u8]) -> Option<&'a [u8]> {
+        match self {
+            Self::Single { seq_no } if buffer.len() >= NAK_SMALL_SIZE => {
+                buffer[0..4].copy_from_slice(&seq_no.to_le_bytes());
+                Some(buffer.split_at(NAK_SMALL_SIZE).0)
+            }
+            Self::Multiple { loss_data } if buffer.len() >= NAK_BIG_SIZE => {
+                buffer[0..4].copy_from_slice(&loss_data[0].to_le_bytes());
+                buffer[4..8].copy_from_slice(&loss_data[1].to_le_bytes());
+                Some(buffer.split_at(NAK_BIG_SIZE).0)
+            }
+            _ => None,
+        }
+    }
+
+    pub fn deserialize(buffer: &mut [u8]) -> Option<Self> {
+        if buffer.len() == NAK_BIG_SIZE {
+            Some(Self::Multiple {
+                loss_data: [
+                    u32::from_le_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]),
+                    u32::from_le_bytes([buffer[4], buffer[5], buffer[6], buffer[7]]),
+                ],
+            })
+        } else if buffer.len() == NAK_SMALL_SIZE {
+            Some(Self::Single {
+                seq_no: u32::from_le_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]),
+            })
+        } else {
+            None
+        }
+    }
+}
+
 /// Acknowledgement packet control info
 #[derive(Debug, Copy, Clone)]
 pub struct AckControlInfo {
     /// The packet sequence number to which all the
     /// previous packets have been received (excluding)
-    pub received_last_ack: i32,
+    pub received_last_ack: u32,
     /// Optional additional info
     pub info: Option<AckAdditionalInfo>,
+}
+
+impl AckControlInfo {
+    pub fn serialize<'a>(&self, buffer: &'a mut [u8]) -> Option<&'a [u8]> {
+        let mut total_size = ACK_SMALL_SIZE;
+        if buffer.len() < total_size {
+            return None;
+        }
+
+        // 1) 32 bits: The packet sequence number to which all the previous packets
+        //    have been received (excluding)
+        buffer[0..4].copy_from_slice(&self.received_last_ack.to_le_bytes());
+
+        if let Some(info) = &self.info {
+            total_size = ACK_MEDIUM_SIZE;
+            if buffer.len() < total_size {
+                return None;
+            }
+
+            // 2) 32 bits: RTT (in microseconds)
+            buffer[4..8].copy_from_slice(&info.rtt.to_le_bytes());
+
+            // 3) 32 bits: RTT variance
+            buffer[8..12].copy_from_slice(&info.rtt_var.to_le_bytes());
+
+            // 4) 32 bits: Available buffer size (in bytes)
+            buffer[12..16].copy_from_slice(&info.buffer_size.to_le_bytes());
+
+            if let Some((speed, bandwidth)) = &info.speed_and_bandwidth {
+                total_size = ACK_BIG_SIZE;
+                if buffer.len() < total_size {
+                    return None;
+                }
+
+                // 5) 32 bits: Packets receiving rate (in number of packets per second)
+                buffer[16..20].copy_from_slice(&speed.to_le_bytes());
+
+                // 6) 32 bits: Estimated link capacity (in number of packets per second)
+                buffer[20..24].copy_from_slice(&bandwidth.to_le_bytes());
+            }
+        }
+
+        Some(buffer.split_at(total_size).0)
+    }
+
+    pub fn deserialize(buffer: &[u8]) -> Option<Self> {
+        if buffer.len() < ACK_SMALL_SIZE {
+            return None;
+        }
+
+        // 1) 32 bits: The packet sequence number to which all the previous packets
+        //    have been received (excluding)
+        let received_last_ack = u32::from_le_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]);
+
+        let info = if buffer.len() == ACK_SMALL_SIZE {
+            None
+        } else if buffer.len() == ACK_MEDIUM_SIZE || buffer.len() == ACK_BIG_SIZE {
+            // 2) 32 bits: RTT (in microseconds)
+            let rtt = u32::from_le_bytes([buffer[4], buffer[5], buffer[6], buffer[7]]);
+
+            // 3) 32 bits: RTT variance
+            let rtt_var = u32::from_le_bytes([buffer[8], buffer[9], buffer[10], buffer[11]]);
+
+            // 4) 32 bits: Available buffer size (in bytes)
+            let buffer_size = u32::from_le_bytes([buffer[12], buffer[13], buffer[14], buffer[15]]);
+
+            let speed_and_bandwidth = if buffer.len() == ACK_BIG_SIZE {
+                Some((
+                    // 5) 32 bits: Packets receiving rate (in number of packets per second)
+                    u32::from_le_bytes([buffer[16], buffer[17], buffer[18], buffer[19]]),
+                    // 6) 32 bits: Estimated link capacity (in number of packets per second)
+                    u32::from_le_bytes([buffer[20], buffer[21], buffer[22], buffer[23]]),
+                ))
+            } else {
+                None
+            };
+
+            Some(AckAdditionalInfo {
+                rtt,
+                rtt_var,
+                buffer_size,
+                speed_and_bandwidth,
+            })
+        } else {
+            return None;
+        };
+
+        Some(Self {
+            received_last_ack,
+            info,
+        })
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -107,13 +257,13 @@ pub struct HandshakeControlInfo {
 }
 
 impl HandshakeControlInfo {
-    pub fn serialize(&self, buffer: &mut [u8]) -> bool {
+    pub fn serialize<'a>(&self, buffer: &'a mut [u8]) -> Option<&'a [u8]> {
         if buffer.len() < HANDSHAKE_SIZE {
-            return false;
+            return None;
         }
 
         // 1) 32 bits: UDT version
-        buffer[0..8].copy_from_slice(&[UDT_VERSION, 0, 0, 0]);
+        buffer[0..4].copy_from_slice(&[UDT_VERSION, 0, 0, 0]);
 
         // 2) 32 bits: Socket Type (STREAM or DGRAM)
         buffer[4..8].copy_from_slice(&[
@@ -151,7 +301,7 @@ impl HandshakeControlInfo {
         buffer[44..48].copy_from_slice(&self.ip[3].to_le_bytes());
 
         // Done
-        true
+        Some(buffer.split_at(HANDSHAKE_SIZE).0)
     }
 
     pub fn deserialize(buffer: &[u8]) -> Option<Self> {
@@ -219,3 +369,12 @@ pub enum SocketType {
 
 const UDT_VERSION: u8 = 4;
 const HANDSHAKE_SIZE: usize = 48;
+
+const ACK_SMALL_SIZE: usize = 4;
+const ACK_MEDIUM_SIZE: usize = ACK_SMALL_SIZE + 12;
+const ACK_BIG_SIZE: usize = ACK_MEDIUM_SIZE + 8;
+
+const NAK_SMALL_SIZE: usize = 4;
+const NAK_BIG_SIZE: usize = 8;
+
+const MDR_SIZE: usize = 8;
